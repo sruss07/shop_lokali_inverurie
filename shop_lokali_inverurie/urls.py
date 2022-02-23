2bc1b4ffc35aca3e3bab6f8dc012ac5039459378
sruss07 <srussell1903@yahoo.co.uk>
2022-02-23 17:23:56 +0000

    updates to main app urls and settings

--- shop_lokali_inverurie/urls.py
+++ shop_lokali_inverurie/urls.py
@@ -1,7 +1,6 @@
 """shop_lokali_inverurie URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +13,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
+    path('products/', include('products.urls')),
+    path('blog/', include('blog.urls')),
+    path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
+    path('review/', include('review.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
